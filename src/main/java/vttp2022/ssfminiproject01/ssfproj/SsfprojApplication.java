34517f04588ecc1dde318a80d92ebd397b53a77c
zawan1997 <nawaz.shss@gmail.com>
2022-08-15 14:52:07 +0800

    ssf proj first commit

---
+++ src/main/java/vttp2022/ssfminiproject01/ssfproj/SsfprojApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.ssfminiproject01.ssfproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssfminiproject01/ssfproj/SsfprojApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssfminiproject01.ssfproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
